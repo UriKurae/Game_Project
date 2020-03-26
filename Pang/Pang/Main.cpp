c3d89bb7d9f5d4fe15788ed617d73de80c5ca05d
LucasPG14 <lucaspg14@gmail.com>
2020-03-26 20:40:28 +0100

    Updated Structure

--- Pang/Pang/Main.cpp
+++ Pang/Pang/Main.cpp
@@ -3,8 +3,8 @@
 #include "MemLeaks.h"
 
 #include "External_Libraries/SDL/include/SDL.h"
-#pragma comment( lib, "SDL/libx86/SDL2.lib")
-#pragma comment( lib, "SDL/libx86/SDL2main.lib")
+#pragma comment( lib, "External_Libraries/SDL/libx86/SDL2.lib")
+#pragma comment( lib, "External_Libraries/SDL/libx86/SDL2main.lib")
 
 
 enum class main_states
