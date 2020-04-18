7765c3295bec10db7c6e07724e6ddb3845c067c9
UriKurae <youtfrost@gmail.com>
2020-04-18 19:23:42 +0200

    Must fix music

--- Pang/Pang/Source/IntroScene.cpp
+++ Pang/Pang/Source/IntroScene.cpp
@@ -26,7 +26,7 @@ bool SceneIntro::Start()
 	bool ret = true;
 
 	bgTexture = App->textures->Load("Assets/UI/Intro.png");
-	App->audio->PlayMusic("Assets/Music/introTitle.ogg", 1.0f);
+	App->audio->PlayMusic("Assets/Sound/Sounds_Gameplay/Title.ogg", 1.0f);
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
